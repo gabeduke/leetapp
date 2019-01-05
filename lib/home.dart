afa51b2740e2e19c3c109352d2890c9a14625250
gabeduke <gabeduke@gmail.com>
2019-01-05 01:16:48 -0500

    clean up stale code, implement swipeable list

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,9 @@
 import 'dart:async';
-import 'dart:io';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:app/main.dart';
-import 'package:app/text.dart';
 import 'package:app/drawer.dart';
-import 'package:app/const.dart';
 import 'package:app/settings.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -24,19 +19,16 @@ class MyHomePage extends StatefulWidget {
 
 class MyHomePageState extends State<MyHomePage> {
 
-  String displayData;
-  String appUser;
+  List<String> litems = [];
+  final TextEditingController eCtrl = new TextEditingController();
+
   Map<String, String> data;
-  StreamSubscription<DocumentSnapshot> subscription;
   bool isLoading = false;
 
   List<Choice> choices = const <Choice>[
     const Choice(title: 'Settings', icon: Icons.settings),
     const Choice(title: 'Log out', icon: Icons.exit_to_app),
   ];
-
-  final CollectionReference collectionReference =
-      Firestore.instance.collection("users");
 
   final GoogleSignIn googleSignIn = new GoogleSignIn();
 
@@ -57,60 +49,6 @@ class MyHomePageState extends State<MyHomePage> {
     } else {
       Navigator.push(context, MaterialPageRoute(builder: (context) => Settings()));
     }
-  }
-
-  void _delete() {
-    collectionReference.document(appUser).delete().whenComplete(() {
-      print("Deleted Successfully");
-      setState(() {
-        displayData = "User Deleted";
-      });
-    }).catchError((e) => print(e));
-  }
-
-  void _fetch() {
-    try {
-      collectionReference.document(appUser).get().then((datasnapshot) {
-        if (datasnapshot.exists) {
-          setState(() {
-            displayData = datasnapshot.data['name'];
-          });
-        } else {
-          setState(() {
-            displayData = "User doesn't exist";
-          });
-        }
-      });
-    } catch (e) {
-      print(e.toString());
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    try {
-      appUser = googleSignIn.currentUser.email;
-      subscription = collectionReference.document(appUser).snapshots().listen((datasnapshot) {
-      if (datasnapshot.exists) {
-        setState(() {
-          displayData = datasnapshot.data['email'];
-        });
-      }
-    });
-    } catch (e) {
-      print(e.toString());
-      setState(() {
-        displayData = "Please log in";
-      });
-    };
-
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    subscription?.cancel();
   }
 
   @override
@@ -144,42 +82,47 @@ class MyHomePageState extends State<MyHomePage> {
           ],
         ),
         drawer: LeetDrawer(),
-        body: new Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                new Padding(padding: const EdgeInsets.all(10.0)),
-                new RaisedButton(
-                  onPressed: _delete,
-                  child: new Text("Delete"),
-                  color: Theme.of(context).accentColor,
-                ),
-                new Padding(padding: const EdgeInsets.all(10.0)),
-                new RaisedButton(
-                  onPressed: _fetch,
-                  child: new Text("Get"),
-                  color: Theme.of(context).accentColor,
-                ),
-                new Padding(padding: const EdgeInsets.all(10.0)),
-                RaisedButton(
-                  child: Text('Edit'),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => EditScreen()),
-                    );
-                  },
-                ),
-                new Padding(padding: const EdgeInsets.all(10.0)),
-                displayData == null
-                    ? new Container()
-                    : new Text(
-                        displayData,
-                        style: new TextStyle(fontSize: 20),
-                      ),
-              ],
-            )));
+      body: new Column(
+        children: <Widget>[
+          new TextField(
+            decoration: InputDecoration(
+              icon: Icon(Icons.text_fields),
+              labelText: 'Gift Wiki Wants..',
+            ),
+            controller: eCtrl,
+            onSubmitted: (text) {
+              litems.add(text);
+              eCtrl.clear();
+              setState(() {});
+            },
+          ),
+          new Expanded(
+            child: new ListView.builder
+              (
+                itemCount: litems.length,
+                itemBuilder: (BuildContext ctxt, int index) {
+                  final item = litems[index];
+
+                  return Dismissible(
+                    // Each Dismissible must contain a Key. Keys allow Flutter to
+                    // uniquely identify Widgets.
+                    key: Key(item),
+                    // We also need to provide a function that tells our app
+                    // what to do after an item has been swiped away.
+                    onDismissed: (direction) {
+                      // Remove the item from our data source.
+                      setState(() {
+                        litems.removeAt(index);
+                      });
+
+                      // Then show a snackbar!
+                      // Scaffold.of(context)
+                      //     .showSnackBar(SnackBar(content: Text("$item dismissed")));
+                      }, child: ListTile(title: Text('$item')),
+                  );
+                })
+          )],
+      )
+            );
   }
 }
